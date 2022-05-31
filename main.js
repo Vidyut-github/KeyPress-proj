aec0e0eea643ab218011de6bc094c76e8a4d0b8b
Vidyut-github <83949337+Vidyut-github@users.noreply.github.com>
2022-05-31 18:36:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+var canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+rover_width=100;
+rover_height=90;
+rover_x=150;
+rover_y=150;
+mars_array=["mars.jpg","mars1.jpeg","mars2.jpeg","mars3.jpeg"];
+var random_number=Math.floor(Math.random()*4);
+var background_image=mars_array[random_number];
+var rover_image="rover.png";
+console.log(random_number);
+function add() {
+    background_imageTag=new Image();
+    background_imageTag.onload=uploadBackground;
+    background_imageTag.src=background_image;
+    rover_imageTag=new Image();
+    rover_imageTag.onload=uploadRover;
+   rover_imageTag.src=rover_image;
+}
+function uploadBackground() {
+    ctx.drawImage(background_imageTag,0,0,canvas.width,canvas.height);
+}
+function uploadRover() {
+    ctx.drawImage(rover_imageTag,rover_x,rover_y,rover_width,rover_height);
+} 
+window.addEventListener("keydown",mykeydown);
+function mykeydown(e) {
+    var keypressed=e.keyCode;
+    console.log(keypressed);
+    if (keypressed=="38") {
+        up();
+        console.log("up");
+    }
+    if (keypressed=="40") {
+        down();
+        console.log("down");
+    }
+    if (keypressed=="37") {
+        left();
+        console.log("left");
+    }
+    if (keypressed=="39") {
+        right();
+        console.log("right"); 
+    }
+}
+function up() {
+    if (rover_y>=0) {
+        rover_y=rover_y-10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function down() {
+    if (rover_y<=600) {
+        rover_y=rover_y+10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function left() {
+    if (rover_x>=0) {
+        rover_x=rover_x-10;
+        uploadBackground();
+        uploadRover();
+    }
+}function right() {
+    if (rover_x<=800) {
+        rover_x=rover_x+10;
+        uploadBackground();
+        uploadRover();
+    }
+}
